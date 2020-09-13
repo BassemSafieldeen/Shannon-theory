287c57089157bf32a522507803d6bfeac32ac37e
BassemSafieldeen <bassemsafieldeen@gmail.com>
2020-09-13 20:50:22 +0200

    Add stuff

---
+++ src/FSMC.lean
@@ -0,0 +1,13 @@
+---- FINITE STATE-MACHINE CHANNEL
+
+/-
+Finite state-machine channel (FSMC)
+-/
+structure FSMC : Type :=
+(PFA : )
+(output_alphabet : )
+
+
+instance FSMC : FSMC_B_alpha
+
+instance FSMC : FSMC_B_alpha'
---
+++ src/Turing_meets_Shannon.lean
@@ -0,0 +1,8 @@
+-- https://arxiv.org/pdf/2008.13270.pdf
+-- https://www.nature.com/articles/s41467-018-03428-0
+-- Your master's thesis
+
+/-
+This is about uncomputability of capacities of classical 
+channels.
+-/
